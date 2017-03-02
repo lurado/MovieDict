60e45b7114f527f606074d0b56ca3a2a796fb3a8
Julian Raschke <julian@raschke.de>
2017-03-02 22:04:07 +0800

    Make Database generation more robust

--- Database/suggestion.rb
+++ Database/suggestion.rb
@@ -3,6 +3,8 @@ task :fill_suggestion_column => :database do
   counter = 0
   indices = {}
   
+  $db.execute "UPDATE movies SET suggestion = NULL"
+  
   $db.execute2 "SELECT * FROM movies" do |columns|
     if indices.empty?
       columns.each_with_index do |column_name, index|
@@ -12,14 +14,10 @@ task :fill_suggestion_column => :database do
       id = columns[indices['id']]
       languages = LANGUAGES.map { |lang| columns[indices[lang]] }.compact
       
-      if languages.count < 4
-        suggestion = "NULL"
-      else
-        suggestion = counter.to_s
+      if languages.count >= 4
+        $db.execute "UPDATE movies SET suggestion = #{counter} WHERE id = ?", id
         counter += 1
       end
-
-      $db.execute "UPDATE movies SET suggestion = #{suggestion} WHERE id = ?", id
     end
   end
   
